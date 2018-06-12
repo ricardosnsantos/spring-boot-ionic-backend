7e50b43f2a15e0e1fc5e83cc6ec00a8cac0d8aad
Ricardo Santos <ricardo.snsantos.m@gmail.com>
2018-06-12 08:08:54 -0300

    Ajustes no endpoint / categorias / {id}

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -11,16 +11,17 @@ import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@Autowired
 	private CategoriaService service;
-	
-	@RequestMapping(value="{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id){
-		
+
+	@RequestMapping(value = "{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+
 	}
 }
